433bd9e326ff80dccad4592e5ef87cf774d47d82
benjaminlgur <benjaminlgur@yahoo.com>
2017-11-18 20:55:06 -0500

    Add files via upload

--- solve.cpp
+++ solve.cpp
@@ -11,6 +11,8 @@
 
 const char brute[26] = {'e', 't', 'a', 'o', 'i', 'n', 's', 'h', 'r', 'd', 'l', 'c', 'u', 'm', 'w', 'f', 'g', 'y', 'p', 'b', 'v', 'k', 'j', 'x', 'q', 'z'}; //array populated based on order of letter prevalence in English.
 
+const char titanium[8] = {'t','i','t','a','n','i','u','m'};
+
 using namespace std;
 
 // Brute-force search for one letter
@@ -23,89 +25,50 @@ void guess(Puzzle &puzzle, int row, int col){
     }
 }
 
+/*void titaniumWord(Puzzle &puzzle, int r, int c){
+    puzzle.guess(r, c + 1, 'a');
+    puzzle.guess(r, c + 2, 'n');
+    puzzle.guess(r, c + 3, 'i');
+    puzzle.guess(r, c + 4, 'u');
+    puzzle.guess(r, c + 5, 'm');
+}*/
+
 /* The main function solving the crossword puzzle */
 void solve(Puzzle &puzzle){
-    int wordSize[100];
-    int wordCount = 0;
-    int spaceSize[100];
-    int spaceCount = 0;
-    char order[500];
-    for (int i = 0; i < 500; i++){
-        order[i] = 'n';
-    }
-    int orderNumber = 0;
-    //Determining puzzle contents.
-    enum Condition { start, space, letter, endline };
-    Condition cond = start; 
-    for (int r = 0; r < puzzle.height; r++) {
+    int wordSize = 0;
+    int soFar = 0;
+    for (int r = 0; r < puzzle.height; r++){
         for (int c = 0; c < puzzle.width; c++){
-            if(puzzle.get(r, c) == ' '){
-                if (cond == space){
-                    spaceSize[spaceCount]++;
-                }
-                else if (cond == letter || cond == endline){
-                    cond = space;
-                    order[orderNumber] =  ' ';
-                    orderNumber++;
-                    spaceCount++;
-                    spaceSize[spaceCount]++;
-                }
-                else if (cond == start){
-                    cond = space;
-                    order[orderNumber] =  ' ';
-                    orderNumber++;
-                    spaceSize[spaceCount]++;
-                }
+            if(puzzle.get(r,c) == '?'){
+                wordSize++;
+                soFar++;
             }
-            if(puzzle.get(r, c) == '?'){
-                if(cond == letter){
-                    wordSize[wordCount]++;
-                }
-                else if (cond == space || cond == endline){
-                    cond = letter;
-                    order[orderNumber] =  '?';
-                    orderNumber++;
-                    wordCount++;
-                    wordSize[wordCount]++;
-                }
-                else if(cond == start){
-                    cond = letter;
-                    order[orderNumber] =  '?';
-                    orderNumber++;
-                    wordSize[wordCount]++;
-                }
+            if(puzzle.get(r,c) == ' '){
+                soFar++;
+                wordSize = 0;
             }
-        }
-    }
-    int col = 0;
-    int row = 0;
-    int x = 0;
-    int wordPos = 0;
-    int spacePos = 0;
-    while(order[x] == ' '){
-        for(int i = 0; i < spaceSize[spacePos]; i++){
-            row++;
-            if (row > puzzle.width){
-                row = 0;
-                col++;
-            }
-        }
-        spacePos++;
-        x++;
-    }
-    while(order[x] == '?'){
-        if(wordSize[wordPos] < 4){
-            for(int i = 0; i < wordSize[wordPos]; i++){
-                guess(puzzle, row, col);
+            if (wordSize < 4){
+                guess(puzzle, r, c);
                 puzzle.print();
-                row++;
-                if (row > puzzle.width){
-                    row = 0;
-                    col++;
+            }
+            if (wordSize >= 4){
+                //Start
+                if(puzzle.get(r , c - 1) == 't' && puzzle.get(r , c - 2) == 'i' && puzzle.get (r, c - 3) == 't'){
+                    puzzle.guess(r, c + 1, 'a');
+                    puzzle.guess(r, c + 2, 'n');
+                    puzzle.guess(r, c + 3, 'i');
+                    puzzle.guess(r, c + 4, 'u');
+                    puzzle.guess(r, c + 5, 'm');
+                    c = c + 5;
                 }
+                
+                //End
+            }
+            if (soFar >= puzzle.width) {
+                wordSize = 0;
+                soFar = 0;
             }
         }
-        wordPos++;
-        x++;
-    }               
+    }
 }
+            
